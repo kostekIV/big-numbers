25596fb9bb6a4672ac137289a5ba28f079edc177
kostekIV <27210860+kostekIV@users.noreply.github.com>
2021-02-19 01:29:14 +0100

    Add ordering (#18)

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use std::cmp::Ordering;
+
+use crate::asm_ops::cmp_slices;
+
 #[inline]
 pub(crate) fn trim_zeros(x: &mut Vec<u64>) {
     while let Some(v) = x.last() {
@@ -19,6 +23,25 @@ pub(crate) fn bit_len(x: u64) -> u32 {
     }
 
     i
+}
+
+#[inline]
+pub(crate) fn cmp_repr(left: &[u64], right: &[u64]) -> Ordering {
+    let (n, m) = (left.len(), right.len());
+
+    match n.cmp(&m) {
+        Ordering::Less => return Ordering::Less,
+        Ordering::Greater => return Ordering::Greater,
+        Ordering::Equal => {
+            let cmp_res = unsafe { cmp_slices(left.as_ptr(), right.as_ptr(), n as u64) };
+            match cmp_res {
+                -1 => return Ordering::Less,
+                0 => return Ordering::Equal,
+                1 => return Ordering::Greater,
+                _ => panic!("Unexpected result"),
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -66,5 +89,42 @@ mod tests {
         for input in test_inputs.iter() {
             assert_eq!(bit_len(input.0), input.1);
         }
+    }
+
+    #[test]
+    fn cmp_repr_test() {
+        let test_inputs = (
+            ([1, 2, 3, 4], [], Ordering::Greater),
+            ([1, 2, 3, 4], [1, 2], Ordering::Greater),
+            ([], [], Ordering::Equal),
+            ([1], [5], Ordering::Less),
+            ([4], [1, 2], Ordering::Less),
+            ([1, 2, 3, 4], [1, 2, 3, 3], Ordering::Greater),
+        );
+
+        assert_eq!(
+            test_inputs.0 .2,
+            cmp_repr(&test_inputs.0 .0, &test_inputs.0 .1)
+        );
+        assert_eq!(
+            test_inputs.1 .2,
+            cmp_repr(&test_inputs.1 .0, &test_inputs.1 .1)
+        );
+        assert_eq!(
+            test_inputs.2 .2,
+            cmp_repr(&test_inputs.2 .0, &test_inputs.2 .1)
+        );
+        assert_eq!(
+            test_inputs.3 .2,
+            cmp_repr(&test_inputs.3 .0, &test_inputs.3 .1)
+        );
+        assert_eq!(
+            test_inputs.4 .2,
+            cmp_repr(&test_inputs.4 .0, &test_inputs.4 .1)
+        );
+        assert_eq!(
+            test_inputs.5 .2,
+            cmp_repr(&test_inputs.5 .0, &test_inputs.5 .1)
+        );
     }
 }
